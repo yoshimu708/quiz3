a254d0f7b051c69c58a16e4d8969e437b67f6e36
yoshimu708 <73528183+yoshimu708@users.noreply.github.com>
2021-02-15 21:39:48 +0900

    javaでの要素生成

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-'use strict'
+'use strict';
 
 {
+  const question= document.getElementById('question');
+  const choices= document.getElementById('choices');
+  const btn= document.getElementById('btn');
 
+  const quizSet = [
+    {q: 'What is A?', c:['a0','a1','a2']},
+    {q: 'What is B?', c:['b0','b1','b2']},
+    {q: 'What is C?', c:['a0','a1','a2']},
+  ];
+  let currentNum = 0;
+
+  question.textContent = quizSet[currentNum].q;
+
+  quizSet[currentNum].c.forEach(choice =>{
+    const li= document.createElement('li');
+    li.textContent = choice;
+    choices.appendChild(li);
+  });
 }
